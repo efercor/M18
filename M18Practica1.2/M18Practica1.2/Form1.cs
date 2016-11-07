8b68a6456d4b884c2a66e1d08d98416dba1475de
Oscar <osanfelix@outlook.es>
2016-11-07 15:41:35 +0100

    Borrada mas €%%#### de Miguel

--- M18Practica1.2/M18Practica1.2/Form1.cs
+++ M18Practica1.2/M18Practica1.2/Form1.cs
@@ -10,9 +10,12 @@ using System.Windows.Forms;
 
 namespace M18Practica1._2
 {
+	
     public partial class Form1 : Form
     {
-        public Form1()
+		Form2 ventanaPaciente;
+		
+		public Form1()
         {
             InitializeComponent();
         }
@@ -21,5 +24,21 @@ namespace M18Practica1._2
         {
 
         }
-    }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			ventanaPaciente = new Form2();
+			ventanaPaciente.ShowDialog();
+		}
+	}
 }
